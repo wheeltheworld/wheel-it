537c8ff339cb0eac214bda78dfccf172b8fd8a90
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2022-06-28 20:04:31 -0400

    AMS-160 feat: Adding configuration for new package name @wheeltheworld/wheel-it.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
-import { terser } from "rollup-plugin-terser";
-import pkg from "./package.json";
 import resolve from "@rollup/plugin-node-resolve";
-import copy from "rollup-plugin-copy";
 import tsb from "rollup-plugin-ts";
+
+import pkg from "./package.json";
 
 const buildDir = "dist";
 
@@ -14,42 +13,14 @@ const bundle = ({ name, path }) => ({
   ],
   output: [
     {
-      file: `./${buildDir}/${name}.js`,
-      format: "cjs",
+      file: `./${buildDir}/${name}.esm.js`,
+      format: "esm",
       sourcemap: true,
-    },
+    }
   ],
   plugins: [
     tsb(),
     resolve(),
-    terser({
-      output: {
-        comments: false,
-      },
-    }),
-    copy({
-      targets: [
-        { src: "README.md", dest: buildDir },
-        {
-          src: "package.json",
-          dest: buildDir,
-          transform: (content) => {
-            const {
-              scripts,
-              devDependencies,
-              husky,
-              release,
-              engines,
-              module,
-              main,
-              types,
-              ...keep
-            } = JSON.parse(content.toString());
-            return JSON.stringify(keep, null, 2);
-          },
-        },
-      ],
-    }),
   ],
 });
 
